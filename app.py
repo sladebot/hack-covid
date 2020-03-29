60ff6e5412973e86b8f6beb501c023f708ea42dc
Souranil Sen <souranil@unity3d.com>
2020-03-29 02:11:30 -0700

    prod

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from app import init_app
 
-app = init_app(os.getenv('FLASK_CONFIG') or 'development')
+app = init_app(os.getenv('FLASK_CONFIG') or 'production')
 
 if __name__ == "__main__":
     app.run()
