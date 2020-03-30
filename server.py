75982f21d31d4304330b70e861fbfeaa6db36cc7
Souranil Sen <souranil@unity3d.com>
2020-03-29 18:41:50 -0700

    beanstalk stuff

--- server.py
+++ server.py
@@ -2,10 +2,7 @@ import os
 
 from app import init_app
 
-app = init_app(os.getenv('FLASK_CONFIG') or 'production')
+application = init_app(os.getenv('FLASK_CONFIG') or 'production')
 
 if __name__ == "__main__":
-    app.run()
-
-def heroku():
-    app.run()
+    application.run()
