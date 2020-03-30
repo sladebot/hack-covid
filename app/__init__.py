125e1de89e16d5108a65de83fd12274a619b62d3
Souranil Sen <souranil@unity3d.com>
2020-03-29 18:54:53 -0700

    Works

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,15 @@
 from flask import Flask
-from flask import make_response, render_template, jsonify, url_for
+from flask import make_response, jsonify
 from app.config.config import config
 from app.sentiment_analyser import SentimentAnalyser
 from app.consumer import RssConsumer
 from app.generator import RssGenerator
-from flask import request
 from app.giphy import GiphyAPI
 
 def init_app(config_name):
     app = Flask(__name__)
     rss_consumer = RssConsumer()
-    feeds_analyser = SentimentAnalyser()
     app.config.from_object(config[config_name])
-    giphy = GiphyAPI()
 
 
     @app.route('/')
@@ -35,28 +32,5 @@ def init_app(config_name):
         res.headers.set('Content-Type', 'application/rss+xml')
 
         return res
-
-    @app.route('/sentiment-analysis')
-    def analyse_feed():
-        feeds =  [{
-            "title" : "Some Title",
-            "description" : "This is some terrible terrible description. I hate this."
-        },
-        {
-            "title" : "Positive title",
-            "description" : "This is a positive news."
-        }]#request.args.get('feed')
-        #analysis = feeds_analyser.get_sentiment_analysis(feed)
-        analysis = feeds_analyser.get_sentiment_scores(feeds)
-        return analysis
-
-
-    @app.errorhandler(500)
-    def server_error(error=None):
-        return render_template('500.html')
-
-    @app.errorhandler(404)
-    def not_found(error=None):
-        return render_template('404.html')
 
     return app
--- app/generator/__init__.py
+++ app/generator/__init__.py
@@ -8,7 +8,7 @@ class RssGenerator:
         fg.title("Sanity")
         fg.description("An emotionally balanced newsfeed")
         fg.language("en")
-        fg.link(href='', rel='alternate')
+        fg.link(href='https://example.com', rel='alternate')
         for a in articles:
             fe = fg.add_entry()
             fe.title(a["title"])
